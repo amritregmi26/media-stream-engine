3fe9b88a24810c384d35c83b48cb2a941b71111a
amritregmi26 <amrregmi@gmail.com>
2026-06-10 22:27:02 +0545

    fix: missing middlewares

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.monitor import router as monitor_router
 from app.api.v1.auth import router as auth_router
 
 
 def get_application():
     _app = FastAPI()
+
+    _app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     _app.include_router(router=monitor_router, prefix="/api/v1")
     _app.include_router(router=auth_router, prefix="/api/v1")
